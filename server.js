28755c4fc46dd31a358eefe624046913ae379804
Bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-01-06 14:26:31 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(MONGO_URI)
+        console.log('Connected to MongoDb');
+    } catch (e) {
+        console.log('MongoDb not connected');
+    }
+}
+
+start()
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+})
