3dbb954c1cd122e06820fad6d8e80bdc6e19db2d
Tudor Comaniciu <tmcomaniciu@gmail.com>
2024-01-07 19:04:07 -0500

    connected to MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,24 @@
 import express from "express";
 const { Request, Response } = express;
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/api/test", async (req, res) => {
-  res.json({ message: "express endpoint" });
+  try {
+    res.json({ message: "express endpoint" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
 app.listen(3000, () => {
