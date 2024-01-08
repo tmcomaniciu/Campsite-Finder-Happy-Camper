afd6295f14b8a730ca0c3b34747c1ad1daeb0a10
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-01-08 15:23:50 -0500

    adding update and delete route in camp controller

--- Controllers/camp_controllers.js
+++ Controllers/camp_controllers.js
@@ -12,7 +12,41 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const camp = await Camp.findById(id)
+        res.json(camp)
+    } catch (error) {
+        console.log('Error', error)
+        res.status(500).json({ message: 'error getting particular camp site'})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const camp = await Camp.findByIdAndUpdate(id, {...req.body}, {new: true})
+        res.json(camp)
+    } catch(error) {
+        console.log('Error', error)
+        res.status(500).json({ message: 'error updating a camp site'})
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    try {
+        const { id } = req.params
+        const camp = await Camp.findByIdAndDelete(id)
+        res.json(camp)
+    } catch(error) {
+        console.log('error', error)
+        res.status(500).json({ message: 'error in deleting a camp'})
+    }
+})
+
 router.post('/', async (req, res) => {
+    console.log(req.body);
     try {
         await new Camp({...req.body}).save()
         res.status(200).json({message: 'camp created'})
