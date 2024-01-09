1c331071e2642d930ecf49edcd6db1ba4a2232cd
LaneyS05 <laney.staggs05@gmail.com>
2024-01-09 13:19:51 -0500

    adding background img

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,15 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <Hero />
-      <div className="container mx-auto py-10 flex-1">
-        {children}
-      </div>
+      <div className="container mx-auto py-10 flex-1">{children}</div>
       <Footer />
     </div>
   );
