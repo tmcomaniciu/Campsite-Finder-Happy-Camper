a6e82d5344f66d20666e0983308fe070fcc23cf7
LaneyS05 <laney.staggs05@gmail.com>
2024-01-08 21:14:29 -0500

    adding styling and react

---
+++ camp/src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+//import tree from "./tree.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="welcome">start your next adventur today</h1>
+        <nav className="navbar">
+          <ul>
+            <li>
+              <a>home</a>
+            </li>
+            <li>
+              <a>sites</a>
+            </li>
+            <li>
+              <a>book</a>
+            </li>
+            <li>
+              <a>activities</a>
+            </li>
+          </ul>
+        </nav>
+        <p>
+          Get ready to explore the outdors with freinds or family; Book your
+          campsite today
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
