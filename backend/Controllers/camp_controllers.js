dd88d10cadb2200ab56a761535cfcc5a20f9a313
bhuvaneswari630 <bhuvaneswarim630@gmail.com>
2024-01-10 09:47:42 -0500

    Adding seeds data

--- backend/Controllers/camp_controllers.js
+++ backend/Controllers/camp_controllers.js
@@ -3,12 +3,21 @@ const Camp = require('../Models/Camp')
 const router = require('express').Router()
 
 router.get('/', async (req, res) => {
+    const searchBy = Object.keys(req.query)[0]
+    const searchByValue = req.query[searchBy]
     try {
-        const camp = await Camp.find()
-        res.json(camp)
-    } catch(error) {
+        if (searchBy === undefined) {
+            // console.log()
+            const camp = await Camp.find()
+            res.json(camp)
+        } else {
+            const camp = await Camp.find({ state: { $eq: searchByValue } })
+            res.json(camp)
+        }
+
+    } catch (error) {
         console.log('Error', error);
-        res.status(500).json({message: 'error getting all camp sites'})
+        res.status(500).json({ message: 'error getting all camp sites' })
     }
 })
 
@@ -19,40 +28,40 @@ router.get('/:id', async (req, res) => {
         res.json(camp)
     } catch (error) {
         console.log('Error', error)
-        res.status(500).json({ message: 'error getting particular camp site'})
+        res.status(500).json({ message: 'error getting particular camp site' })
     }
 })
 
 router.put('/:id', async (req, res) => {
     try {
         const { id } = req.params
-        const camp = await Camp.findByIdAndUpdate(id, {...req.body}, {new: true})
+        const camp = await Camp.findByIdAndUpdate(id, { ...req.body }, { new: true })
         res.json(camp)
-    } catch(error) {
+    } catch (error) {
         console.log('Error', error)
-        res.status(500).json({ message: 'error updating a camp site'})
+        res.status(500).json({ message: 'error updating a camp site' })
     }
 })
 
-router.delete('/:id', async (req,res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const { id } = req.params
         const camp = await Camp.findByIdAndDelete(id)
         res.json(camp)
-    } catch(error) {
+    } catch (error) {
         console.log('error', error)
-        res.status(500).json({ message: 'error in deleting a camp'})
+        res.status(500).json({ message: 'error in deleting a camp' })
     }
 })
 
 router.post('/', async (req, res) => {
     console.log(req.body);
     try {
-        await new Camp({...req.body}).save()
-        res.status(200).json({message: 'camp created'})
+        await new Camp({ ...req.body }).save()
+        res.status(200).json({ message: 'camp created' })
     } catch (error) {
         console.log('Error', error);
-        res.status(500).json({message: 'error creating camp site'})
+        res.status(500).json({ message: 'error creating camp site' })
     }
 
 })
--- backend/Models/Camp.js
+++ backend/Models/Camp.js
@@ -1,11 +1,35 @@
 const mongoose = require('mongoose')
 
 const CampSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    location: { type: String, required: true },
-    description: { type: String, required: true },
-    imageURL: { type: String, required: false },
-    price: { type: Number, required: true }
+    name: {
+        type: String, required: true
+    },
+    city: {
+        type: String, required: true
+    },
+    state: {
+        type: String, required: true
+    },
+    geolocation: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    description: {
+        type: String, required: true
+    },
+    imageURL: {
+        type: String, required: false
+    },
+    price: {
+        type: Number, required: true
+    }
 })
 
 module.exports = mongoose.model('Camp', CampSchema)
